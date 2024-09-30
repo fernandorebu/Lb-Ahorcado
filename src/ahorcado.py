eb1bd900735eb7e986bf14917b3bbbebc8f6d813
fernandorebu <fernandorebu123@gmail.com>
2024-09-30 12:33:09 +0200

    Segunda clase de laboratorio

--- src/ahorcado.py
+++ src/ahorcado.py
@@ -10,6 +10,9 @@ def cargar_palabras(ruta):
         for linea in f:
             res.append(linea.strip()) # strip() elimina los espacios en blanco y saltos de línea al principio y al final
         return res
+    # Control S para Guardar
+
+
 
 def elegir_palabra(palabras):
     '''
@@ -20,8 +23,11 @@ def elegir_palabra(palabras):
     - La función 'random.choice' del paquete 'random' recibe una lista de opciones y 
       devuelve una de ellas seleccionada aleatoriamente.
     '''
+    #aleatorio = random.choice(palabras)
+    #print("La palabra aleatoria es: " , aleatorio)
     return random.choice(palabras)
-    
+
+
 def enmascarar_palabra(palabra, letras_probadas):
     '''
     Enmascarar la palabra:
@@ -33,17 +39,15 @@ def enmascarar_palabra(palabra, letras_probadas):
     - Utilice el método join de las cadenas. Observe el siguiente ejemplo:
         ' '.join(['a','b','c']) # Devuelve "a b c"
     '''
-    lista_caracteres = [] # Inicializar una lista vacía
-    for letra in palabra: # Recorrer cada letra de la palabra
-        # Si la letra está en la lista letras_probadas
-        if letra in letras_probadas:                   # ESCRIBIR CONDICIÓN
-            # Añadir a lista_caracteres la letra
-            lista_caracteres.append(letra)       # COMPLETAR
+    lista = []
+    for letra in palabra:
+        if letra in letras_probadas:
+            lista.append(letra)
         else:
-            # Añadir a lista_caracteres un guión bajo '_'
-            lista_caracteres.append('_')       # COMPLETAR
-    
-    return ' '.join(lista_caracteres)
+            lista.append("_")
+    return ' '.join(lista)
+
+
 
 def pedir_letra(letras_probadas):
     '''
@@ -56,10 +60,13 @@ def pedir_letra(letras_probadas):
     - La función 'input' permite leer una cadena de texto desde la entrada estándar
     - El método 'lower' aplicado a una cadena devuelve una copia de la cadena en minúsculas
     '''
-    letra = input("Di una letra:").lower()
+    letra = input("Escriba una letra:").lower()
     while letra in letras_probadas:
-        letra = input("Esa ya la dijiste, di otra letra:").lower()
+            letra = input("Escriba una letra:").lower()
     return letra
+
+
+
 
 def comprobar_letra(palabra_secreta, letra):
     '''
@@ -68,9 +75,47 @@ def comprobar_letra(palabra_secreta, letra):
     - Mostrar el mensaje correspondiente informando al usuario
     - Devolver True si estaba y False si no
     '''
+
     if letra in palabra_secreta:
-        print("¡Letra correcta!")
+        print("Has acertaso")
         return True
     else:
-        print("Esa letra no es correcta :-(")
+        print("Fallaste")
         return False
+    
+
+
+def comprobar_palabra_completa(palabra_secreta, letras_probadas):
+    '''
+    Comprobar si se ha completado la palabra:
+    - Comprobar si todas las letras de la palabra secreta han sido propuestas por el usuario
+    - Devolver True si es así o False si falta alguna letra por adivinar
+    '''
+    for letra in palabra_secreta:
+        if letra not in letras_probadas:
+            return False
+        
+
+    return True
+        
+
+
+def ejecutar_turno(palabra_secreta, letras_probadas):
+    '''
+    Ejecutar un turno de juego:
+    - Mostrar la palabra enmascarada
+    - Pedir la nueva letra
+    - Comprobar si la letra está en la palabra (acierto) o no (fallo)
+    - Añadir la letra al conjunto de letras probadas
+    - Devolver True si la letra fue un acierto, False si fue un fallo
+    Ayuda:
+    - Recuerda las funciones que ya has implementado para mostrar la palabra, pedir la letra y comprobarla
+    '''
+    palabra_enmascarada = enmascarar_palabra(palabra_secreta, letras_probadas)
+    print("La palabra que tienes que adivinar es ", palabra_enmascarada)
+    #pedir la nueva letra
+    letra = pedir_letra(letras_probadas)
+    #Comprobar si la letra esta en la palabra 
+    letras_probadas.add(letra)
+    return comprobar_letra(palabra_secreta, letra)
+
